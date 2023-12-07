d78668a43e1f8e64edc411bb7d413597dfe4a4ba
solemode404 <146377506+solemode404@users.noreply.github.com>
2023-12-07 13:28:01 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index page</title>
+    </head>
+    <body>
+        <?php include './nav.php'?>
+       <h1>Index page</h1>
+    </body>
+</html>
