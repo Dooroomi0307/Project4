d68d28dccf1ed8933cecea1cae69328228bb33b3
solemode404 <146377506+solemode404@users.noreply.github.com>
2023-12-08 14:11:29 -0500

    added about changes

--- frontend/about.php
+++ frontend/about.php
@@ -6,7 +6,17 @@
     </head>
     <body>
     <?php include './nav.php'?>
-       <h1>about page</h1>
+    <div id="about-container">
+        <div id="img-box">
+        <img src="./resources/try.png" id="image">
+        <div class="centered">A real estate company with a purpose.</div>
+        </div>
+        <hr>
+        <div id="about-text">
+            <p>Comp Realtors is building the first modern real estate platform,<br> pairing the industry’s top talent with 
+                technology to make the<br> search and sell experience intelligent and seamless.</p>
+        </div>
+       </div>
     </body>
 
 </html>
