47bfd3fa5afcbe1fd8d404e303cb286af2ccaf72
solemode404 <146377506+solemode404@users.noreply.github.com>
2023-12-10 11:50:24 -0500

    removed view more button from staff

--- frontend/staff.php
+++ frontend/staff.php
@@ -22,9 +22,7 @@
       <span class="card-title">Realtor</span>
       <span class="card-name"> Goldfinger </span>
       <p class="card-text">No potential client, Goldfinger does not expect you to die during your process of buying your first house. He hopes to do whatever possible to help you!</p>
-      <form action="../Swipe/Patricia.html">
-          <button class="card-btn" onclick="">View more</button>
-      </form>
+    
   </div>
 </div>
 <div class="card swiper-slide">
@@ -36,9 +34,7 @@
       <span class="card-title">Realtor</span>
       <span class="card-name"> Blofeld</span>
       <p class="card-text">You only buy your first house once and Blofeld, our most notorious Realtor will do what he can to ensure your experience is a memorable one!</p>
-      <form action="../Swipe/Jonathan.html">
-          <button class="card-btn" onclick="">View more</button>
-      </form>
+     
   </div>
 </div>
 <div class="card swiper-slide">
@@ -50,9 +46,7 @@
       <span class="card-title">Realtor</span>
       <span class="card-name"> Chiffre </span>
       <p class="card-text">Chiffre, most famous for his lookalike in Casino Royale, is a go-getter when it comes to finding the right house for you.</p>
-      <form action="../Swipe/Benjamin.html">
-          <button class="card-btn" onclick="">View more</button>
-      </form>
+    
   </div>
 </div>
 <div class="card swiper-slide">
@@ -64,9 +58,7 @@
       <span class="card-title">Realtor</span>
       <span class="card-name"> General Ourumov</span>
       <p class="card-text">Our General is sure to provide you with an amazing service as he has a Goldeneye when it comes to details to get you into the right house!</p>
-      <form action="../Swipe/Heather.html">
-          <button class="card-btn" onclick="">View more</button>
-      </form>
+     
   </div>
 </div>
 <div class="card swiper-slide">
@@ -78,9 +70,7 @@
       <span class="card-title">Realtor</span>
       <span class="card-name"> Xenia Onatopp</span>
       <p class="card-text">A fitness enthusiast, Xenia is known for her enthusiastic personality when interacting with her clients, and provides an enjoyable experience throughout!</p>
-      <form action="../Swipe/Brandy.html">
-          <button class="card-btn" onclick="">View more</button>
-      </form>
+     
   </div>
 </div>
 
