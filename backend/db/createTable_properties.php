2aecd6a18020d863e3b8c7d5d50696597f6eb9e3
roycel6 <129793658+roycel6@users.noreply.github.com>
2023-12-10 13:07:13 -0500

    add config.php

--- backend/db/createTable_properties.php
+++ backend/db/createTable_properties.php
@@ -1,18 +1,6 @@
 <?php
 //REMEMBER: USE YOUR OWN LOGIN WHEN UPLOADING TO SERVER
-$host = "localhost"; 
-$user = ""; 
-$pass = ""; 
-$dbname = "";
-
-// Create connection
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-// Check connection
-if($conn->connect_error) {
-	echo "Could not connect to server\n";
-	die("Connection failed: " . $conn->connect_error);
-}
+require("config.php");
 // Create table
 $createTable = "CREATE TABLE IF NOT EXISTS properties (
     id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
