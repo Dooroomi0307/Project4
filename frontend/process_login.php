eeb7b879de6407d154dd4c28cf7a1d672119d816
roycel6 <129793658+roycel6@users.noreply.github.com>
2023-12-10 01:56:41 -0500

    add process login

---
+++ frontend/process_login.php
@@ -0,0 +1,39 @@
+<?php
+require('config.php');
+session_start();
+
+
+	if(isset($_POST["username"])){
+			$username = $_POST["username"];
+		}
+		
+	if(isset($_POST["password"])){
+			$password = $_POST["password"];
+		}
+		
+	if(isset($_POST["remember"])){
+			$remember = $_POST["remember"];
+		}
+	
+	if($remember==1){
+		setcookie('uname', $username, time()+60*60, "/");	//cookies will last an hour
+		setcookie('upass', $password, time()+60*60, "/");
+	}
+	
+	
+	$sql = "SELECT Password FROM userdata WHERE Username = '$username'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_array($result);
+	$count = mysqli_num_rows($result);
+	$hash = $row[0];
+	
+	if(password_verify($password, $hash)){
+		$_SESSION['user'] = $username;
+		header("location: frontend/search/src/search.php");
+	} else{
+		header("location: incorrect_login.html");
+	}
+	
+?>
+
+
